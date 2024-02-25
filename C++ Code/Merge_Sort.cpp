030d594629b3f2050d2300fb74d452f44ae2226a
Christopher yang <146152532+Cyang18@users.noreply.github.com>
2024-02-24 16:22:57 -0900

    Merge Sort Code
    C++ version of merge sort

---
+++ C++ Code/Merge_Sort.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <chrono>
+
+using namespace std::chrono;
+using namespace std;
+void merge(vector<int>& arr, int left, int mid, int right);
+
+void merge(vector<int>& arr, int left, int mid, int right)
+    {
+        int start_index_left = left;
+        int start_index_right = mid + 1;
+        int temp_element = left;
+        int temp[500000];
+
+
+        while (start_index_left <= mid && start_index_right <= right)
+        {
+            if (arr[start_index_left] < arr[start_index_right])
+            {
+                temp[temp_element] = arr[start_index_left];
+                start_index_left++ ;
+            }
+            else {
+                temp[temp_element] = arr[start_index_right] ;
+                start_index_right++ ;
+            }
+            temp_element++ ;
+        }
+
+
+        if (start_index_left > mid)
+        {
+            while (start_index_right <= right)
+            {
+                temp[temp_element] = arr[start_index_right];
+                temp_element++ ;
+                start_index_right++ ;
+            }
+        }
+
+        else {
+
+            while (start_index_left <= mid)
+            {
+                temp[temp_element] = arr[start_index_left];
+                temp_element++ ;
+                start_index_left++ ;
+            }
+        }
+
+    for (int temp_element = left; temp_element <= right; temp_element++)
+    {
+        arr[temp_element] = temp[temp_element] ;
+    }
+}
+
+void mergeSort(vector<int>& arr, int left, int right)
+   {
+       if (left < right)
+       {
+           int mid = 0;
+           mid = (left + right) / 2;
+           mergeSort(arr, left, mid);
+           mergeSort(arr, mid + 1, right) ;
+           merge(arr, left, mid, right);
+       }
+   }
+
+int main()
+{
+
+    vector<int> array;
+    int size;
+    cout<<"Enter a size: ";
+    cin>>size;
+    for(int i=0; i < size; i++)
+    {
+        array.push_back(rand() % 100);
+        array.push_back(array[i]);
+    }
+    auto start = high_resolution_clock::now();
+    mergeSort(array, 0, size-1);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Merge sort time is: " << duration.count() << " microseconds" << endl;
+
+    //prints the list to make sure the algorithm sorts the list properly
+    for (int j = 0; j < size; j++)
+    {
+        cout << ' ' << array[j];
+
+    }
+
+    cout << endl;
+    return 0;
+}
