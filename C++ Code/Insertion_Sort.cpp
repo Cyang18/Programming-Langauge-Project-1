eada622b5a6857b20d7476c0410fb0263a8197fb
Christopher yang <146152532+Cyang18@users.noreply.github.com>
2024-03-14 23:09:37 -0800

    Update Insertion_Sort.cpp
    Add a change, where you now get the cpu and the memory usage. But we are using mb and ms so make sure you convert your outputs after.

--- C++ Code/Insertion_Sort.cpp
+++ C++ Code/Insertion_Sort.cpp
@@ -2,23 +2,43 @@
 #include <cstdlib>
 #include <vector>
 #include <chrono>
+#include <ctime>
+#include <sys/resource.h>
+#include <sys/time.h>
 
 using namespace std::chrono;
 using namespace std;
 
-void insertionSort(vector<int>& arr, int size)
+// Function to measure CPU time
+double getCPUTime()
+{
+    struct rusage rusage;
+    getrusage(RUSAGE_SELF, &rusage);
+    struct timeval utime = rusage.ru_utime;
+    struct timeval stime = rusage.ru_stime;
+    return (utime.tv_sec + utime.tv_usec / 1000000.0) + stime.tv_sec + stime.tv_usec / 1000000.0;
+}
 
+// Function to measure memory usage returns usage in kilobytes
+long getMemoryUsage()
+{
+    struct rusage rusage;
+    getrusage(RUSAGE_SELF, &rusage);
+    return rusage.ru_maxrss; 
+}
+
+void insertionSort(vector<int>& arr, int size)
 {
     for (int i = 1; i < size; i++)
     {
         int current = arr[i];
-        int temp = i-1;
+        int temp = i - 1;
         while (temp >= 0 && arr[temp] > current)
         {
-            arr[temp+1] = arr[temp];
+            arr[temp + 1] = arr[temp];
             temp--;
         }
-        arr[temp+1] = current;
+        arr[temp + 1] = current;
     }
 }
 
@@ -27,24 +47,27 @@ int main()
 
     vector<int> array;
     int size;
-    cout<<"Enter a size: ";
-    cin>>size;
-    for(int i=0; i < size; i++)
+    cout << "Enter a size: ";
+    cin >> size;
+    for (int i = 0; i < size; i++)
     {
         array.push_back(rand() % 100);
         array.push_back(array[i]);
     }
     auto start = high_resolution_clock::now();
-    insertionSort(array,size);
+    double cpu_start = getCPUTime();
+    insertionSort(array, size);
     auto stop = high_resolution_clock::now();
+    double cpu_stop = getCPUTime();
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "Insertion sort time is: " << duration.count() << " microseconds" << endl;
+    cout << "CPU time used: " << (cpu_stop - cpu_start) << " seconds" << endl;
+    cout << "Memory usage: " << getMemoryUsage() << " KB" << endl;
 
-    //prints the list to make sure the algorithm sorts the list properly
+    // prints the list to make sure the algorithm sorts the list properly
     for (int j = 0; j < size; j++)
     {
         cout << ' ' << array[j];
-
     }
 
     cout << endl;
