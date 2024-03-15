448e5265c759fb971b24c79ce05a365eadf43f00
Christopher yang <146152532+Cyang18@users.noreply.github.com>
2024-03-15 01:48:43 -0800

    Update Quick_sort.cpp
    same update like the last two

--- C++ Code/Quick_sort.cpp
+++ C++ Code/Quick_sort.cpp
@@ -2,13 +2,37 @@
 #include <cstdlib>
 #include <vector>
 #include <chrono>
+#include <sys/resource.h>
+#include <sys/time.h>
 
 using namespace std::chrono;
 using namespace std;
-void swap(int &x, int &y);
-void quickSort(vector<int> & arr, int low, int high);
 
-void quickSort(vector<int> & arr, int low, int high)
+double getCPUTime()
+{
+    struct rusage rusage;
+    getrusage(RUSAGE_SELF, &rusage);
+    struct timeval utime = rusage.ru_utime;
+    struct timeval stime = rusage.ru_stime;
+    return utime.tv_sec + utime.tv_usec / 1000000.0 + stime.tv_sec + stime.tv_usec / 1000000.0;
+}
+
+long getMemoryUsage()
+{
+    struct rusage rusage;
+    getrusage(RUSAGE_SELF, &rusage);
+    return rusage.ru_maxrss;
+}
+
+void swap(int &x, int &y)
+{
+    int temp;
+    temp = x;
+    x = y;
+    y = temp;
+}
+
+void quickSort(vector<int> &arr, int low, int high)
 {
     if (low < high)
     {
@@ -19,10 +43,9 @@ void quickSort(vector<int> & arr, int low, int high)
         {
             if (arr[i] <= pivot)
             {
-                temp++; 
+                temp++;
                 swap(arr[temp], arr[i]);
             }
-
         }
         swap(arr[temp + 1], arr[high]);
         partition = (temp + 1);
@@ -30,16 +53,6 @@ void quickSort(vector<int> & arr, int low, int high)
         quickSort(arr, low, partition - 1);
         quickSort(arr, partition + 1, high);
     }
-
-}
-
-void swap(int &x, int &y)
-{
-
-    int temp;
-    temp = x;
-    x = y;
-    y = temp;
 }
 
 int main()
@@ -47,27 +60,31 @@ int main()
 
     vector<int> array;
     int size;
-    cout<<"Enter a size: ";
-    cin>>size;
-    for(int i=0; i < size; i++)
+    cout << "Enter a size: ";
+    cin >> size;
+    for (int i = 0; i < size; i++)
     {
         array.push_back(rand() % 100);
         array.push_back(array[i]);
     }
 
     auto start = high_resolution_clock::now();
+    double cpu_start = getCPUTime();
     quickSort(array, 0, size - 1);
+    double cpu_stop = getCPUTime();
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "Quick sort time is: " << duration.count() << " microseconds" << endl;
+    cout << "CPU time used: " << (cpu_stop - cpu_start) << " seconds" << endl;
+    cout << "Memory usage: " << getMemoryUsage() << " KB" << endl;
 
-    //prints the list to make sure the algorithm sorts the list properly
+    // prints the list to make sure the algorithm sorts the list properly
     for (int j = 0; j < size; j++)
     {
         cout << ' ' << array[j];
-
     }
 
     cout << endl;
     return 0;
 }
+
