3264ca984e52266d13db08040551ea49fe9bdecc
Christopher yang <146152532+Cyang18@users.noreply.github.com>
2024-03-16 01:37:53 -0800

    Update and rename Java Insertion Sort to InsertionSort.java
    Re-fixed the code, to allow rand set size. And get the cpu and memory usage.

---
+++ Java/InsertionSort.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.time.Duration;
+import java.time.Instant;
+
+public class InsertionSort {
+
+    // Function to measure CPU time
+    static double getCPUTime() {
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
+        long start = System.nanoTime();
+        long startCpuTime = runtime.totalMemory() - runtime.freeMemory();
+        return (double) (start + startCpuTime) / 1e9;
+    }
+
+    // Function to measure memory usage returns usage in kilobytes
+    static long getMemoryUsage() {
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
+        return (runtime.totalMemory() - runtime.freeMemory()) / 1024;
+    }
+
+    static void insertionSort(ArrayList<Integer> arr) {
+        int size = arr.size();
+        for (int i = 1; i < size; i++) {
+            int current = arr.get(i);
+            int temp = i - 1;
+            while (temp >= 0 && arr.get(temp) > current) {
+                arr.set(temp + 1, arr.get(temp));
+                temp--;
+            }
+            arr.set(temp + 1, current);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a size: ");
+        int size = scanner.nextInt();
+
+        ArrayList<Integer> array = new ArrayList<>();
+
+        Random rand = new Random();
+        for (int i = 0; i < size; i++) {
+            array.add(rand.nextInt(100));
+        }
+
+        Instant start = Instant.now();
+        double cpu_start = getCPUTime();
+        insertionSort(array);
+        Instant stop = Instant.now();
+        double cpu_stop = getCPUTime();
+
+        Duration duration = Duration.between(start, stop);
+
+        System.out.println("Insertion sort time is: " + duration.toNanos() + " nanoseconds");
+        System.out.println("CPU time used: " + (cpu_stop - cpu_start) + " seconds");
+        System.out.println("Memory usage: " + getMemoryUsage() + " KB");
+
+        // prints the list to make sure the algorithm sorts the list properly
+        for (int j = 0; j < size; j++) {
+            System.out.print(" " + array.get(j));
+        }
+
+        System.out.println();
+    }
+}
+
