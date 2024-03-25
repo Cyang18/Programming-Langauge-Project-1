442d8987806cdab38506891e6f65e084c3d7a1cc
Christopher yang <146152532+Cyang18@users.noreply.github.com>
2024-03-25 14:56:10 -0800

    Update Insertion_Sort.py
    Changed to the code conversion. Change the library to psutil to help get the memory and cpu usuage

--- python code/Insertion_Sort.py
+++ python code/Insertion_Sort.py
@@ -1,6 +1,7 @@
+import psutil
 import random
 import time
-import resource
+#import resource
 
 def insertion_sort(arr):
     for i in range(1, len(arr)):
@@ -13,20 +14,23 @@ def insertion_sort(arr):
 
 if __name__ == "__main__":
     size = int(input("Enter a size: "))
-    array = [random.randint(0, 99) for _ in range(size)]
+    array = ["place array values here"]
 
     start = time.time()
-    cpu_start = resource.getrusage(resource.RUSAGE_SELF).ru_utime + resource.getrusage(resource.RUSAGE_SELF).ru_stime
-
     # function call
     insertion_sort(array)
     stop = time.time()
-    cpu_stop = resource.getrusage(resource.RUSAGE_SELF).ru_utime + resource.getrusage(resource.RUSAGE_SELF).ru_stime
 
+    # Get cpu usuage
+    cpu_usage = psutil.cpu_percent()
+
+    # Get memory usage
+    memory_usage = psutil.virtual_memory().used / 1024 
+    
     # print the statements for exe, cpu, and mem
     print("Insertion sort time is:", (stop - start) * 1e6, "microseconds")
-    print("CPU time used:", (cpu_stop - cpu_start) * 1e6, "microseconds")
-    print("Memory usage:", resource.getrusage(resource.RUSAGE_SELF).ru_maxrss, "KB")
+    print("CPU time used:", cpu_usage, "%")
+    print("Memory usage:", memory_usage, "KB")
 
     # prints the list to make sure the algorithm sorts the list properly
     print("Sorted array:", array)
