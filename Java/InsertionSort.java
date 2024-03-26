81a620330fbeefeb8586ee06f82fe75f9515d7f4
Ckane55 <161179424+Ckane55@users.noreply.github.com>
2024-03-25 16:48:40 -0800

    Update InsertionSort.java

--- Java/InsertionSort.java
+++ Java/InsertionSort.java
@@ -4,20 +4,12 @@ import java.time.Instant;
 
 public class InsertionSort {
 
-    // Function to measure CPU time
-    static double getCPUTime() {
-        Runtime runtime = Runtime.getRuntime();
-        runtime.gc();
-        long start = System.nanoTime();
-        long startCpuTime = runtime.totalMemory() - runtime.freeMemory();
-        return (double) (start + startCpuTime) / 1e9;
-    }
 
     // Function to measure memory usage returns usage in kb
     static long getMemoryUsage() {
         Runtime runtime = Runtime.getRuntime();
         runtime.gc();
-        return (runtime.totalMemory() - runtime.freeMemory()) / 1024;
+        return (runtime.totalMemory() - runtime.freeMemory());
     }
 
     static void insertionSort(ArrayList<Integer> arr) {
@@ -35,26 +27,37 @@ public class InsertionSort {
 
     public static void main(String[] args) {
 
-        // Define your static array here
-        ArrayList<Integer> array = new ArrayList<>(Arrays.asList("Put your values here"));
+        Scanner scanner = new Scanner(System.in);
 
-        Instant start = Instant.now();
-        double cpu_start = getCPUTime();
+        System.out.print("Enter a size: ");
+        int size = scanner.nextInt();
+
+        ArrayList<Integer> array = new ArrayList<>();
+        Random rand = new Random();
+        for (int i = 0; i < size; i++) {
+            array.add(rand.nextInt(100));
+
+
+        }
+
+        long start = System.nanoTime();
+        
 
         // function call
         insertionSort(array);
-        Instant stop = Instant.now();
-        double cpu_stop = getCPUTime();
+        long stop = System.nanoTime();
+        
+        long duration = stop - start;
+        
 
-        Duration duration = Duration.between(start, stop);
-
-        // prints the statements for the exe, cpu, and mem.
-        System.out.println("Insertion sort time is: " + (duration.toNanos() / 1000) + " microseconds");
-        System.out.println("CPU time used: " + ((cpu_stop - cpu_start) * 1e6) + " microseconds");
+       
+        System.out.println("Insertion sort time is: " + (duration / 1000) + " microseconds");
+       
         System.out.println("Memory usage: " + getMemoryUsage() + " KB");
+       
 
         // prints the list to make sure the algorithm sorts the list properly
-        for (int j = 0; j < array.size(); j++) {
+        for (int j = 0; j < size; j++) {
             System.out.print(" " + array.get(j));
         }
 
