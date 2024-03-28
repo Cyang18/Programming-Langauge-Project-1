c1b5cea4c9515e23edbc48b82b2b08fd18bc7fa9
Christopher yang <146152532+Cyang18@users.noreply.github.com>
2024-03-27 16:58:05 -0800

    Update File IO Read.java
    I just moved the read file portion of the code to its own function. And add a function call in the main to keep track of when the file read began to access the file.

--- Java/File IO Read.java
+++ Java/File IO Read.java
@@ -4,25 +4,49 @@ import java.io.FileNotFoundException;
 
 
 public class read{
+
     static long startTime = System.nanoTime();
-    public static void main(String[] args){
-        try{
-            File object = new File("output.txt");
-            Scanner reader = new Scanner(object);
-            while(reader.hasNextLine()){
+
+    // Function to measure memory usage returns usage in kb
+    static long getMemoryUsage()
+    {
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
+        return (runtime.totalMemory() - runtime.freeMemory());
+    }
+
+    // Function to read file
+    static void readFile(String fileName)
+    {
+        try {
+            File file = new File(fileName);
+            Scanner reader = new Scanner(file);
+
+            while (reader.hasNextLine()) {
                 String data = reader.nextLine();
-                //System.out.println(data);
+                System.out.println(data);
 
             }
             reader.close();
-            
-        } catch (FileNotFoundException e){
+
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        long endTime = System.nanoTime();
-        long duration = (endTime - startTime);
-        System.out.println(duration / 1000);
     }
-  
+
+    public static void main(String[] args) {
+        String fileName = "output.txt";
+
+        long start = System.nanoTime();
+        readFile(fileName);
+        long stop = System.nanoTime();
+
+        long duration = stop - start;
+
+
+        System.out.println("Insertion sort time is: " + (duration / 1000) + " microseconds");
+        System.out.println("Memory usage: " + getMemoryUsage() + " KB");
+
+    }
 }
 
