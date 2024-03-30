b92d5c3c700d518dd3f33447f7d413b5b7f19b87
Christopher yang <146152532+Cyang18@users.noreply.github.com>
2024-03-30 15:48:40 -0800

    Update math_op.cpp

--- C++ Code/math_op.cpp
+++ C++ Code/math_op.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <cmath>
 
-void performMathOperations() 
-{
+using namespace std;
+
+void performMathOperations() {
     int a = 100;
     int b = 5;
     int result_addition = a + b;
@@ -10,7 +11,7 @@ void performMathOperations()
     int result_multiplication = a * b;
     int result_division = a / b;
     int result_exponentiation = pow(a, b);
-    double result_sqrt = sqrt(a);
+    double result_sqrt = sqrt(static_cast<double>(a));
 
     long c = 1221348482;
     long d = 583747585;
@@ -20,7 +21,7 @@ void performMathOperations()
     long result_multiplication2 = c * d;
     long result_division2 = c / d;
     long result_exponentiation2 = pow(c, d);
-    double result_sqrt2 = sqrt(d);
+    double result_sqrt2 = sqrt(static_cast<double>(d));
     double result3 = sqrt(result_sqrt2);
 
     double x = 0.5734633;
@@ -38,6 +39,29 @@ void performMathOperations()
     long lots_add2 = c + c + c + c + c + c + d + d + d + d + d + d + d;
     long lots_mult2 = c * c * c * c * c * c * c * c * c * d * d * d * d * d * d;
 
+    cout << "Result Addition: " << result_addition << endl;
+    cout << "Result Subtraction: " << result_subtraction << endl;
+    cout << "Result Multiplication: " << result_multiplication << endl;
+    cout << "Result Division: " << result_division << endl;
+    cout << "Result Exponentiation: " << result_exponentiation << endl;
+    cout << "Result Square Root: " << result_sqrt << endl;
+    cout << "Result Addition 2: " << result_addition2 << endl;
+    cout << "Result Subtraction 2: " << result_subtraction2 << endl;
+    cout << "Result Multiplication 2: " << result_multiplication2 << endl;
+    cout << "Result Division 2: " << result_division2 << endl;
+    cout << "Result Exponentiation 2: " << result_exponentiation2 << endl;
+    cout << "Result Square Root 2: " << result_sqrt2 << endl;
+    cout << "Result 3: " << result3 << endl;
+    cout << "Result Addition 3: " << result_addition3 << endl;
+    cout << "Result Subtraction 3: " << result_subtraction3 << endl;
+    cout << "Result Multiplication 3: " << result_multiplication3 << endl;
+    cout << "Result Division 3: " << result_division3 << endl;
+    cout << "Result Exponentiation 3: " << result_exponentiation3 << endl;
+    cout << "Result Exponentiation 4: " << result_exponentiation4 << endl;
+    cout << "Lots Mult: " << lots_mult << endl;
+    cout << "Lots Add: " << lots_add << endl;
+    cout << "Lots Add 2: " << lots_add2 << endl;
+    cout << "Lots Mult 2: " << lots_mult2 << endl;
 }
 
 int main() {
